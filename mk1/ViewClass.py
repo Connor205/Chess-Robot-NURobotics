f5e2de6d1bde73e93bcc70951d39046ad22b2a29
Connor Nelson <Conn205@gmail.com>
2022-02-19 21:25:37 -0500

    mk1: fixing python 3.10 -> 3.7 dependencies

--- mk1/ViewClass.py
+++ mk1/ViewClass.py
@@ -10,11 +10,15 @@ import logging
 from loggingConstants import formatter, handler
 from Constants import *
 
+
 class View:
     logger = logging.getLogger(__name__)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
-    states = ["WaitingForPlayer", "GettingPlayerMove", "ProcessingPlayerMove", "GettingComputerMove", "MakingComputerMove", "GameOver"]
+    states = [
+        "WaitingForPlayer", "GettingPlayerMove", "ProcessingPlayerMove",
+        "GettingComputerMove", "MakingComputerMove", "GameOver"
+    ]
     revertStates = {
         'ProcessingPlayerMove': 'WaitingForPlayer',
     }
@@ -44,8 +48,9 @@ class View:
                     color = (255, 255, 255)
                 else:
                     color = (0, 0, 0)
-                pg.draw.rect(self.board, color, (i * SQUARE_SIZE, j * SQUARE_SIZE,
-                                            SQUARE_SIZE, SQUARE_SIZE))
+                pg.draw.rect(self.board, color,
+                             (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE,
+                              SQUARE_SIZE))
 
         modifiedFen = fenString.replace(' ', '/', 1)
         ranks = modifiedFen.split('/')
@@ -59,7 +64,7 @@ class View:
                     file += int(char)
                 else:
                     self.board.blit(PIECE_DICTIONARY[char]['image'],
-                               (file * SQUARE_SIZE, rankI * SQUARE_SIZE))
+                                    (file * SQUARE_SIZE, rankI * SQUARE_SIZE))
                     file += 1
                 index += 1
 
@@ -82,24 +87,26 @@ class View:
             if not self.state == self.previousState:
                 self.logger.debug("State changed from {} to {}".format(
                     self.previousState, self.state))
-                match self.state:
-                    case 'WaitingForPlayer':
-                        # Nothing happens here, we dont kick off any concurrency
-                        currentAwait = None
-                    case 'GettingPlayerMove':
-                        currentAwait = ThreadWithReturn(self.gl.getPlayerMove)
-                        currentAwait.start()
-                    case 'ProcessingPlayerMove':
-                        currentAwait = ThreadWithReturn(self.gl.processPlayerMove, previousResult)
-                        currentAwait.start()
-                    case 'GettingComputerMove':
-                        currentAwait = ThreadWithReturn(self.gl.generateComputerMove)
-                        currentAwait.start()
-                    case 'MakingComputerMove':
-                        currentAwait = ThreadWithReturn(self.gl.makeComputerMove, previousResult)
-                        currentAwait.start()
-                    case 'GameOver':
-                        currentAwait = None
+                if self.state == 'WaitingForPlayer':
+                    # Nothing happens here, we dont kick off any concurrency
+                    currentAwait = None
+                if self.state == 'GettingPlayerMove':
+                    currentAwait = ThreadWithReturn(self.gl.getPlayerMove)
+                    currentAwait.start()
+                if self.state == 'ProcessingPlayerMove':
+                    currentAwait = ThreadWithReturn(self.gl.processPlayerMove,
+                                                    previousResult)
+                    currentAwait.start()
+                if self.state == 'GettingComputerMove':
+                    currentAwait = ThreadWithReturn(
+                        self.gl.generateComputerMove)
+                    currentAwait.start()
+                if self.state == 'MakingComputerMove':
+                    currentAwait = ThreadWithReturn(self.gl.makeComputerMove,
+                                                    previousResult)
+                    currentAwait.start()
+                if self.state == 'GameOver':
+                    currentAwait = None
 
                 self.previousState = self.state
             else:
@@ -110,12 +117,18 @@ class View:
                     previousResult = currentAwait.getResult()
                     if not previousResult:
                         if self.state in self.revertStates:
-                            self.logger.error("Error in {}, restarting from {}".format(self.state, self.revertStates[self.state]))
+                            self.logger.error(
+                                "Error in {}, restarting from {}".format(
+                                    self.state, self.revertStates[self.state]))
                             self.state = self.revertStates[self.state]
                             currentState = self.states.index(self.state)
                         else:
-                            self.logger.warning("Error in {}, restarting from beginning of state".format(self.state))
-                            self.previousState = self.states[(currentState - 1) % len(self.states)]
+                            self.logger.warning(
+                                "Error in {}, restarting from beginning of state"
+                                .format(self.state))
+                            self.previousState = self.states[(currentState - 1)
+                                                             %
+                                                             len(self.states)]
                     else:
                         newFen = self.gl.getCurretFenString()
                         if not self.currentFen == newFen:
@@ -123,23 +136,27 @@ class View:
                             newBoardRender = True
                         currentAwait = None
                         currentState = currentState + 1
-                        self.state = self.states[currentState % len(self.states)]
-            
+                        self.state = self.states[currentState %
+                                                 len(self.states)]
+
             if newBoardRender:
                 self.generateBoardFromFen(self.currentFen)
                 newBoardRender = False
             self.screen.blit(self.background, (0, 0))
             self.screen.blit(self.board, (0, 0))
-            self.screen.blit(self.defaultFont.render(f"Frame: {self.frame}", True, (0, 0, 0), (255, 255, 255)), (BOARD_WIDTH, 0))
-            self.screen.blit(self.defaultFont.render(f"State: {self.state}", True, (0, 0, 0), (255, 255, 255)), (BOARD_WIDTH, 20))
-            self.frame = self.frame + 1 
+            self.screen.blit(
+                self.defaultFont.render(f"Frame: {self.frame}", True,
+                                        (0, 0, 0), (255, 255, 255)),
+                (BOARD_WIDTH, 0))
+            self.screen.blit(
+                self.defaultFont.render(f"State: {self.state}", True,
+                                        (0, 0, 0), (255, 255, 255)),
+                (BOARD_WIDTH, 20))
+            self.frame = self.frame + 1
             pg.display.flip()
 
             # loop through events to see if any of them are the escape key
             for event in events:
-                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                if event.type == pg.QUIT or (event.type == pg.KEYDOWN
+                                             and event.key == pg.K_ESCAPE):
                     running = False
-            
-            
-
-
